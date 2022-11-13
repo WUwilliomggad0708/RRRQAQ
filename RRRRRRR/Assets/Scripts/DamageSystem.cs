0a6f23ef6065259b016960f2f0b49496fc9c1254
WUwilliomggad0708 <wuwilliomggad@gmail.com>
2022-11-13 08:41:53 +0800

    7
    爆炸失敗

--- RRRRRRR/Assets/Scripts/DamageSystem.cs
+++ RRRRRRR/Assets/Scripts/DamageSystem.cs
@@ -9,6 +9,25 @@ namespace xin
         [SerializeField, Header("碰到會爆炸的名稱爆炸")]
         private string nameTarget;
 
+        private void OnCollisionEnter2D(Collision collision)
+        {
+            print(collision.gameObject.name);
+            if (collision.gameObject.name.Contains(nameTarget))
+            {
+                Instantiate(prefabExplosion, transform.position, transform.rotation);
+                Destroy(gameObject);
+            }
+
+
+        }
+        private void OnCollisionExit2D(Collision collision)
+        {
+
+        }
+        private void OnCollisionStay2D(Collision collision)
+        {
+
+        }
 
     }
 }
